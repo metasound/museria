84aa9cfc89d5ab8ee47c187645f440767d0026d9
Isaac Campos <info@icampos.me>
2024-03-11 02:20:18 -0600

    Branded Metasound

--- src/browser/face/index.js
+++ src/browser/face/index.js
@@ -1,7 +1,7 @@
-import './styles/main.scss';
+import './styles/main-ms.scss';
 import Akili from 'akili';
 import router from 'akili/src/services/router.js';
-import App from './controllers/app/app.js';
+import App from './controllers/app/app-ms.js';
 import client from './client.js';
 
 App.define();
